ffdd5893a3dae9ae03ac0de970760545b20e4acb
HEnquist <henrik.enquist@gmail.com>
2019-10-14 21:51:45 +0200

    Add arg parsing

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ extern crate rprompt;
 
 use std::process;
 use std::time::{Duration, SystemTime};
-use std::env;
 use std::thread::sleep;
 use std::path::PathBuf;
 use std::cmp::Ordering;
@@ -17,13 +16,14 @@ use serde::{Serialize, Deserialize};
 use std::collections::HashMap;
 use walkdir::WalkDir;
 use chrono::{Local, DateTime, TimeZone};
+use clap::{App, Arg, ArgGroup};
 
 #[derive(Clone, Debug, PartialEq)]
 enum ChangeType {
     Newer,
     Older,
-    Created,
-    Deleted,
+    NewOnly,
+    RefOnly,
     Modified,
 }
 
@@ -313,7 +313,7 @@ fn compare_dirs(dir_new: &DirIndex, dir_ref: &DirIndex) -> Result<HashMap<PathBu
                 diffs.insert(
                         path.to_path_buf(),
                         DiffItem {
-                            diff: ChangeType::Created,
+                            diff: ChangeType::NewOnly,
                             ftype: pathdata_new.ftype,
                             mtime: pathdata_new.mtime,
                         },
@@ -331,7 +331,7 @@ fn compare_dirs(dir_new: &DirIndex, dir_ref: &DirIndex) -> Result<HashMap<PathBu
                 diffs.insert(
                     path.to_path_buf(),
                     DiffItem {
-                        diff: ChangeType::Deleted,
+                        diff: ChangeType::RefOnly,
                         ftype: pathdata_ref.ftype,
                         mtime: pathdata_ref.mtime,
                     },
@@ -351,7 +351,7 @@ fn solve_conflicts(diff_master: &mut HashMap<PathBuf, DiffItem>, diff_copy: &mut
                     | (ChangeType::Newer, ChangeType::Older)
                     | (ChangeType::Older, ChangeType::Newer)
                     | (ChangeType::Older, ChangeType::Older)
-                    | (ChangeType::Created, ChangeType::Created) => {
+                    | (ChangeType::NewOnly, ChangeType::NewOnly) => {
                         //check which is newer, remove oldest
                         if diffitem_master.mtime >= diffitem_copy.mtime {
                             diff_copy.remove(path);
@@ -360,7 +360,7 @@ fn solve_conflicts(diff_master: &mut HashMap<PathBuf, DiffItem>, diff_copy: &mut
                             diff_master.remove(path);
                         }
                     },
-                    (ChangeType::Deleted, ChangeType::Deleted) => {
+                    (ChangeType::RefOnly, ChangeType::RefOnly) => {
                         //fine, remove both
                         diff_copy.remove(path);
                         diff_master.remove(path);
@@ -422,12 +422,12 @@ fn process_queue(mut action_queue: Vec<SyncAction>) -> Result<(), Box<dyn Error>
     Ok(())
 }
 
-fn sync_diffs(diff: &HashMap<PathBuf, DiffItem>, path_src: &PathBuf, path_dest: &PathBuf) -> Result<(), Box<dyn Error>> {
+fn sync_diffs(diff: &HashMap<PathBuf, DiffItem>, path_src: &PathBuf, path_dest: &PathBuf, keep_all: bool) -> Result<(), Box<dyn Error>> {
     let mut actions = Vec::<SyncAction>::new();
     for (path, diffitem) in diff.iter() {
         match diffitem.diff {
             ChangeType::Newer 
-            | ChangeType::Created
+            | ChangeType::NewOnly
             | ChangeType::Modified => {
                 match diffitem.ftype {
                     FileType::Link => {
@@ -442,14 +442,30 @@ fn sync_diffs(diff: &HashMap<PathBuf, DiffItem>, path_src: &PathBuf, path_dest: 
                 }
                 actions.push(SyncAction::CopyMeta {src: translate_path(path, path_src), dest: translate_path(path, path_dest)});
             },
-            ChangeType::Deleted => {
-                match diffitem.ftype {
-                    FileType::Dir => {
-                        actions.push(SyncAction::DeleteDir {dest: translate_path(path, path_dest)});
-                    },
-                    _ => {
-                        actions.push(SyncAction::DeleteFile {dest: translate_path(path, path_dest)});
-                    },
+            ChangeType::RefOnly => {
+                if keep_all {
+                    match diffitem.ftype {
+                        FileType::Link => {
+                            actions.push(SyncAction::CopyLink {src: translate_path(path, path_dest), dest: translate_path(path, path_src)});
+                        },
+                        FileType::Dir => {
+                            actions.push(SyncAction::CopyDir {src: translate_path(path, path_dest), dest: translate_path(path, path_src)});
+                        },
+                        FileType::File => {
+                            actions.push(SyncAction::CopyFile {src: translate_path(path, path_dest), dest: translate_path(path, path_src)});
+                        },
+                    }
+                    actions.push(SyncAction::CopyMeta {src: translate_path(path, path_dest), dest: translate_path(path, path_src)});
+                }
+                else {
+                    match diffitem.ftype {
+                        FileType::Dir => {
+                            actions.push(SyncAction::DeleteDir {dest: translate_path(path, path_dest)});
+                        },
+                        _ => {
+                            actions.push(SyncAction::DeleteFile {dest: translate_path(path, path_dest)});
+                        },
+                    }
                 }
             },
             ChangeType::Older => {
@@ -474,9 +490,20 @@ fn sync_diffs(diff: &HashMap<PathBuf, DiffItem>, path_src: &PathBuf, path_dest: 
 }
 
 // Main loop
-fn watch(path_a: &PathBuf, path_b: &PathBuf, interval: u64) -> Result<(), Box<dyn Error>> {
+fn watch(path_a: &PathBuf, path_b: &PathBuf, interval: Option<u64>, check_only: bool) -> Result<(), Box<dyn Error>> {
 
-    let delay = Duration::from_millis(1000*interval);
+    let mut delay = Duration::from_millis(10000);
+    let mut run_sync_loop = false;
+
+    if let Some(ival) = interval {
+        delay = Duration::from_millis(1000*ival);
+        run_sync_loop = true;
+    }
+
+    if check_only {
+        run_sync_loop = false;
+    }
+    
 
     let mut index_a: DirIndex;
     let mut index_b: DirIndex;
@@ -497,26 +524,32 @@ fn watch(path_a: &PathBuf, path_b: &PathBuf, interval: u64) -> Result<(), Box<dy
         _ => {
             index_a = map_dir(path_a)?;
             index_b = map_dir(path_b)?;
-            println!("No index found, updating B to match A");
-            println!("This will copy everything from\n{}\nto\n{}", path_a.display(), path_b.display());
-            let reply = rprompt::prompt_reply_stdout("Proceed? y or n: ").unwrap();
-            match reply.as_str() {
-                "y" => println!("Syncing..."),
-                _ => {
-                    println!("Exiting");
-                    process::exit(0);
-                }
-            }
+
 
             let diffs = compare_dirs(&index_a, &index_b)?;
-            sync_diffs(&diffs, path_a, path_b)?;
-            index_a = map_dir(path_a)?;
-            index_b = map_dir(path_b)?;
-            save_index(&index_a, &path_a)?;
-            save_index(&index_b, &path_b)?;
-            println!("Done");
+            if !check_only {
+                println!("No index found, merging the contents of A and B");
+                println!("This will sync all content of \n{}\nwith\n{}", path_a.display(), path_b.display());
+                let reply = rprompt::prompt_reply_stdout("Proceed? y or n: ").unwrap();
+                match reply.as_str() {
+                    "y" => println!("Syncing..."),
+                    _ => {
+                        println!("Exiting");
+                        process::exit(0);
+                    }
+                }
+                sync_diffs(&diffs, path_a, path_b, true)?;
+                index_a = map_dir(path_a)?;
+                index_b = map_dir(path_b)?;
+                save_index(&index_a, &path_a)?;
+                save_index(&index_b, &path_b)?;
+                println!("Done");
+            }
 
         }
+    }
+    if !run_sync_loop {
+        return Ok(())
     }
     loop {
         if fs::metadata(&path_a).is_ok() && fs::metadata(&path_b).is_ok() {
@@ -526,8 +559,8 @@ fn watch(path_a: &PathBuf, path_b: &PathBuf, interval: u64) -> Result<(), Box<dy
             diffs_b = compare_dirs(&index_b_new, &index_b)?;
             if !diffs_a.is_empty() || !diffs_b.is_empty() {
                 solve_conflicts(&mut diffs_a, &mut diffs_b)?;
-                sync_diffs(&diffs_a, path_a, path_b)?;
-                sync_diffs(&diffs_b, path_b, path_a)?;
+                sync_diffs(&diffs_a, path_a, path_b, false)?;
+                sync_diffs(&diffs_b, path_b, path_a, false)?;
                 index_a = map_dir(path_a)?;
                 index_b = map_dir(path_b)?;
                 save_index(&index_a, &path_a)?;
@@ -541,12 +574,94 @@ fn watch(path_a: &PathBuf, path_b: &PathBuf, interval: u64) -> Result<(), Box<dy
     }
 }
 
+fn is_valid_path(dir: String) -> Result<(), String> {
+    match PathBuf::from(&dir).canonicalize() {
+        Ok(_) =>  Ok(()),
+        Err(_) => Err(String::from("Invalid path")),
+    }
+}
+
+fn is_valid_uint(val: String) -> Result<(), String> {
+    match val.parse::<usize>() {
+        Ok(_) =>  Ok(()),
+        Err(_) => Err(String::from("Not a positive integer")),
+    }
+}
+
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let path_a = PathBuf::from(&args[1]).canonicalize().unwrap();
-    let path_b = PathBuf::from(&args[2]).canonicalize().unwrap();
-    let interval: u64 = args[3].parse().unwrap();
-    match watch(&path_a, &path_b, interval) {
+    let matches = App::new("TwoWaySync")
+                    .version("0.1.1")
+                    .author("Henrik Enquist <henrik.enquist@gmail.com>")
+                    .about("Sync two directories")
+                    .arg(Arg::with_name("interval")
+                                .short("w")
+                                .long("watch")
+                                .help("Interval in seconds to watch for changes")
+                                .validator(is_valid_uint)
+                                .takes_value(true))
+                    .arg(Arg::with_name("single")
+                                .short("s")
+                                .help("Do a single sync only"))   
+                    .arg(Arg::with_name("check")
+                                .short("c")
+                                .help("Compare and show diff (default)"))   
+                    .group(ArgGroup::with_name("sync")
+                                .args(&["check", "single", "interval"]))      
+                    .arg(Arg::with_name("dir_a")
+                                    .help("First directory")
+                                    .required(true)
+                                    .validator(is_valid_path)
+                                    .index(1))
+                    .arg(Arg::with_name("dir_b")
+                                    .help("Second directory")
+                                    .required(true)
+                                    .validator(is_valid_path)
+                                    .index(2))
+                    .get_matches();
+    if let Some(w) = matches.value_of("interval") {
+        println!("Value for watch: {}", w);
+    }
+
+    if matches.is_present("single") {
+        println!("Run a single sync only");
+    }
+
+    let mut check_only = false;
+    if matches.is_present("check") {
+        println!("Just check diff");
+        check_only = true;
+    }
+
+
+    let path_a: PathBuf;
+    let path_b: PathBuf; 
+
+    if let Some(dir_a) = matches.value_of("dir_a") {
+        path_a = PathBuf::from(&dir_a).canonicalize().unwrap();
+    }
+    else {
+        path_a = PathBuf::from(".").canonicalize().unwrap();
+    }
+
+    if let Some(dir_b) = matches.value_of("dir_b") {
+        path_b = PathBuf::from(&dir_b).canonicalize().unwrap();
+    }
+    else {
+        path_b = PathBuf::from(".").canonicalize().unwrap();
+    }
+
+    let interval: Option<u64>;
+    if let Some(i) = matches.value_of("interval") {
+        interval = Some(i.parse().unwrap());
+    }
+    else {
+        interval = None;
+    }
+    //let args: Vec<String> = env::args().collect();
+    
+    
+    //let interval: u64 = args[3].parse().unwrap();
+    match watch(&path_a, &path_b, interval, check_only) {
         Ok(_) => {},
         Err(e) => {
             println!("Run error {}", e);
